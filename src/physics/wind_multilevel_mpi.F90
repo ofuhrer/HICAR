1ab5e868346512f1105898382ce1e1f723bc9b34
Oliver Fuhrer <oli@ginko.ch>
2026-07-23 00:47:30 +0200

    Add device-staged multilevel halo exchange

--- src/physics/wind_multilevel_mpi.F90
+++ src/physics/wind_multilevel_mpi.F90
@@ -13,6 +13,7 @@ module wind_multilevel_mpi
         integer :: east = MPI_PROC_NULL
         integer :: south = MPI_PROC_NULL
         integer :: north = MPI_PROC_NULL
+        logical :: device_uploaded = .false.
         real(c_double), allocatable :: west_send(:,:), west_recv(:,:)
         real(c_double), allocatable :: east_send(:,:), east_recv(:,:)
         real(c_double), allocatable :: south_send(:,:), south_recv(:,:)
@@ -20,7 +21,10 @@ module wind_multilevel_mpi
     contains
         procedure :: init => init_horizontal_halo_exchange
         procedure :: release => release_horizontal_halo_exchange
+        procedure :: upload_device => upload_horizontal_halo_exchange
+        procedure :: delete_device => delete_horizontal_halo_exchange
         procedure :: exchange => exchange_horizontal_halos
+        procedure :: exchange_device => exchange_horizontal_halos_device
     end type horizontal_halo_exchange_t
 
 contains
@@ -48,6 +52,7 @@ contains
     subroutine release_horizontal_halo_exchange(this)
         class(horizontal_halo_exchange_t), intent(inout) :: this
 
+        call this%delete_device()
         if (allocated(this%west_send)) deallocate(this%west_send, this%west_recv, this%east_send, this%east_recv)
         if (allocated(this%south_send)) deallocate(this%south_send, this%south_recv, this%north_send, this%north_recv)
         this%nx = 0; this%ny = 0; this%nz = 0
@@ -55,6 +60,26 @@ contains
         this%west = MPI_PROC_NULL; this%east = MPI_PROC_NULL
         this%south = MPI_PROC_NULL; this%north = MPI_PROC_NULL
     end subroutine release_horizontal_halo_exchange
+
+
+    subroutine upload_horizontal_halo_exchange(this)
+        class(horizontal_halo_exchange_t), intent(inout) :: this
+
+        if (this%device_uploaded) return
+        !$acc enter data copyin(this%west_send, this%west_recv, this%east_send, this%east_recv, &
+        !$acc                   this%south_send, this%south_recv, this%north_send, this%north_recv)
+        this%device_uploaded = .true.
+    end subroutine upload_horizontal_halo_exchange
+
+
+    subroutine delete_horizontal_halo_exchange(this)
+        class(horizontal_halo_exchange_t), intent(inout) :: this
+
+        if (.not. this%device_uploaded) return
+        !$acc exit data delete(this%west_send, this%west_recv, this%east_send, this%east_recv, &
+        !$acc                  this%south_send, this%south_recv, this%north_send, this%north_recv)
+        this%device_uploaded = .false.
+    end subroutine delete_horizontal_halo_exchange
 
 
     subroutine exchange_horizontal_halos(this, field)
@@ -131,5 +156,108 @@ contains
         end subroutine post_send
 
     end subroutine exchange_horizontal_halos
+
+
+    subroutine exchange_horizontal_halos_device(this, field)
+        class(horizontal_halo_exchange_t), intent(inout) :: this
+        real(c_double), intent(inout) :: field(0:,:,0:)
+        integer, parameter :: tag_to_west = 741, tag_to_east = 742
+        integer, parameter :: tag_to_south = 743, tag_to_north = 744
+        integer :: i, j, k, ierr, n_requests
+        integer :: requests(4), statuses(MPI_STATUS_SIZE,4)
+
+        if (size(field,1) /= this%nx+2 .or. size(field,2) /= this%nz .or. &
+            size(field,3) /= this%ny+2) error stop 'field shape does not match halo exchange'
+        if (.not. this%device_uploaded) error stop 'halo buffers are not on the device'
+
+        !$acc parallel loop gang vector collapse(2) &
+        !$acc present(field,this%west_send,this%east_send)
+        do j = 1, this%ny
+            do k = 1, this%nz
+                this%west_send(k,j) = field(1,k,j)
+                this%east_send(k,j) = field(this%nx,k,j)
+            enddo
+        enddo
+        !$acc update self(this%west_send,this%east_send)
+        this%west_recv = 0.0_c_double
+        this%east_recv = 0.0_c_double
+        n_requests = 0
+        call post_receive(this%west_recv, size(this%west_recv), this%west, tag_to_east)
+        call post_receive(this%east_recv, size(this%east_recv), this%east, tag_to_west)
+        call post_send(this%west_send, size(this%west_send), this%west, tag_to_west)
+        call post_send(this%east_send, size(this%east_send), this%east, tag_to_east)
+        if (n_requests > 0) call MPI_Waitall(n_requests, requests, statuses, ierr)
+        !$acc update device(this%west_recv,this%east_recv)
+        !$acc parallel loop gang vector collapse(2) &
+        !$acc present(field,this%west_recv,this%east_recv)
+        do j = 1, this%ny
+            do k = 1, this%nz
+                if (this%west == MPI_PROC_NULL) then
+                    field(0,k,j) = 0.0_c_double
+                else
+                    field(0,k,j) = this%west_recv(k,j)
+                endif
+                if (this%east == MPI_PROC_NULL) then
+                    field(this%nx+1,k,j) = 0.0_c_double
+                else
+                    field(this%nx+1,k,j) = this%east_recv(k,j)
+                endif
+            enddo
+        enddo
+
+        !$acc parallel loop gang vector collapse(2) &
+        !$acc present(field,this%south_send,this%north_send)
+        do k = 1, this%nz
+            do i = 0, this%nx+1
+                this%south_send(i,k) = field(i,k,1)
+                this%north_send(i,k) = field(i,k,this%ny)
+            enddo
+        enddo
+        !$acc update self(this%south_send,this%north_send)
+        this%south_recv = 0.0_c_double
+        this%north_recv = 0.0_c_double
+        n_requests = 0
+        call post_receive(this%south_recv, size(this%south_recv), this%south, tag_to_north)
+        call post_receive(this%north_recv, size(this%north_recv), this%north, tag_to_south)
+        call post_send(this%south_send, size(this%south_send), this%south, tag_to_south)
+        call post_send(this%north_send, size(this%north_send), this%north, tag_to_north)
+        if (n_requests > 0) call MPI_Waitall(n_requests, requests, statuses, ierr)
+        !$acc update device(this%south_recv,this%north_recv)
+        !$acc parallel loop gang vector collapse(2) &
+        !$acc present(field,this%south_recv,this%north_recv)
+        do k = 1, this%nz
+            do i = 0, this%nx+1
+                if (this%south == MPI_PROC_NULL) then
+                    field(i,k,0) = 0.0_c_double
+                else
+                    field(i,k,0) = this%south_recv(i,k)
+                endif
+                if (this%north == MPI_PROC_NULL) then
+                    field(i,k,this%ny+1) = 0.0_c_double
+                else
+                    field(i,k,this%ny+1) = this%north_recv(i,k)
+                endif
+            enddo
+        enddo
+
+    contains
+
+        subroutine post_receive(buffer, count, source, tag)
+            real(c_double), intent(inout) :: buffer(*)
+            integer, intent(in) :: count, source, tag
+            if (source == MPI_PROC_NULL) return
+            n_requests = n_requests+1
+            call MPI_Irecv(buffer,count,MPI_DOUBLE_PRECISION,source,tag,this%communicator,requests(n_requests),ierr)
+        end subroutine post_receive
+
+        subroutine post_send(buffer, count, destination, tag)
+            real(c_double), intent(in) :: buffer(*)
+            integer, intent(in) :: count, destination, tag
+            if (destination == MPI_PROC_NULL) return
+            n_requests = n_requests+1
+            call MPI_Isend(buffer,count,MPI_DOUBLE_PRECISION,destination,tag,this%communicator,requests(n_requests),ierr)
+        end subroutine post_send
+
+    end subroutine exchange_horizontal_halos_device
 
 end module wind_multilevel_mpi
